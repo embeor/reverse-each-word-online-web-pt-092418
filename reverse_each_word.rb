d0bf1833a3accf6dfada32ee3b6dd01b214e73b4
Emilio Betancourt <embeor@gmail.com>
2018-10-05 03:04:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,9 +7,9 @@ def reverse_each_word (string)
   string_reverse.join (" ")
 end
 
-def reverse_each_word2 (string)
+def reverse_each_word (string)
   split_string = string.split(" ")
     split_string.collect do |word|
-      string_reverse << word.reverse
+      word.reverse
     end
 end
